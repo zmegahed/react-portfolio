83ad1f4cd34b96b88ce1bd6cb7bc6393e9ec745d
zmegahed <zeyadfouad34@gmail.com>
2025-06-04 02:38:11 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+const currentPath = window.location.pathname;
+if (currentPath.endsWith('/') && currentPath !== '/' && currentPath !== '/react-portfolio/') {
+  window.history.replaceState(null, '', currentPath.slice(0, -1));
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-portfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>
